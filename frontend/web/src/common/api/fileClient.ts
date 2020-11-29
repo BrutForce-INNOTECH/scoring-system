0fec6895e0c4a8a715b939ac536e429dda189833
Alexey Nikitin <kvandake@gmail.com>
2020-11-29 11:15:32 +0300

    WIP: Some fix

--- frontend/web/src/common/api/fileClient.ts
+++ frontend/web/src/common/api/fileClient.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 const FILE_TOKEN = "45e9efce34a84a478f298a80b00b6dae";
-const FILE_BASE_URL = 'http://82.148.19.167:25478'
+const FILE_BASE_URL = 'https://brutforce.kvandake.ru'
 
 interface UploadResult {
   ok: boolean;
@@ -23,13 +23,13 @@ class FileClient {
     const formData = new FormData();
     formData.append('file', file, file.name);
 
-    const result = await this.fileClient.post<UploadResult>("/upload", formData, {
+    const result = await this.fileClient.post<UploadResult>("/files/upload", formData, {
       params: {
         token: FILE_TOKEN
       }
     });
 
-    return `${FILE_BASE_URL}/${result.data.path}?token=${FILE_TOKEN}`
+    return `${FILE_BASE_URL}${result.data.path}?token=${FILE_TOKEN}`
   }
 }
 
--- frontend/web/src/pages/api/_constants.ts
+++ frontend/web/src/pages/api/_constants.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const REMOTE_URL = "http://82.148.19.167"
+const REMOTE_URL = "https://brutforce.kvandake.ru"
 
 // Set config defaults when creating the instance
 export const apiClient = axios.create({
